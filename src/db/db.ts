b53e69f473374f98e9bdc41b70958d8f1d6590d8
Jay <jeremyt89@ymail.com>
2024-07-20 01:31:57 -0400

    Initial setup models/db some admin page work

---
+++ src/db/db.ts
@@ -0,0 +1,18 @@
+import {PrismaClient} from '@prisma/client';
+import { Return } from '@prisma/client/runtime/library';
+
+const PrismaClientSingleton = () => {
+    return new PrismaClient();
+}
+
+declare global {
+    var prisma:undefined | ReturnType<typeof PrismaClientSingleton>
+}
+
+const db = globalThis.prisma ?? PrismaClientSingleton()
+
+export default db;
+
+if (process.env.NODE_ENV !== 'production') {
+    globalThis.prisma = db;
+}
